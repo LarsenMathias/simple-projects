510a898083c4d6cd039db3a0c9945f0647ed314f
LarsenMathias <88501525+LarsenMathias@users.noreply.github.com>
2021-08-05 21:30:09 +0530

    Add files via upload

---
+++ small numbers using multiple arguments.py
@@ -0,0 +1,24 @@
+import pyttsx3
+from pyttsx3 import engine
+*engine=pyttsx3
+engine.speak("Enter the first value ")
+a=int(input("Enter the first number " ))
+engine.speak("Enter the second value ")
+b=int(input("Enter the second number "))
+engine.speak("Enter the third value ")
+c=int(input("Enter the third number "))
+engine.speak("Enter the fourth value ")
+d=int(input("Enter the fourth number "))
+if a<b and a<c and a<d:
+    print("The smallest number is ",a)
+    engine.speak("The smallest value is ",a)
+if b<a and b<c and b<d:
+    print("The smallest NUmber is ",b)
+    engine.speak("The smallest value is ",b)
+if c<a and c<b and c<d:
+    print("The samllest number is ",c)
+    engine.speak("The smallest value is ",c)
+if d<a and d<b and d<c:
+    print("The smallest number is ",d)
+    engine.speak("The smallest value is ",d)
+  
